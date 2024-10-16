6bb1261a9e4b86f534a718096df5e911a64c7c6c
aman <amanshaukat@Amans-MacBook-Air-2.local>
2024-10-16 21:01:13 +0100

    AddTripActivity | add a button to add mushroom inside add trip activity

--- app/src/main/java/com/example/mushroomhunting/activity/MainActivity.java
+++ app/src/main/java/com/example/mushroomhunting/activity/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize layout containers instead of buttons
+        // Initialize layout containers
         ConstraintLayout addTripLayout = findViewById(R.id.addTripLayout);
         ConstraintLayout viewTripsLayout = findViewById(R.id.viewTripsLayout);
         ConstraintLayout addMushroomDetailsLayout = findViewById(R.id.addMushroomDetailsLayout);
