1af007081a3fb352484baffa0c553c0882329979
iamAmanShaukat <iamamanshaukat@gmail.com>
2024-10-26 08:43:16 +0100

    splash screen

--- app/src/main/java/com/example/mushroomhunting/activity/MainActivity.java
+++ app/src/main/java/com/example/mushroomhunting/activity/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_MushroomHunting);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
